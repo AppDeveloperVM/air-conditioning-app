c58eddcf09daad31cb5341a0f41191486f5f4576
AppDeveloperV <vicmr1994@gmail.com>
2020-09-16 09:06:59 +0200

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getState() {
+    return this.http.get('');
+  }
 
 }
