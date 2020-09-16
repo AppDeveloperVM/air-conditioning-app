60e549c6375548c2b3814f0d921a321ab177ea7f
AppDeveloperV <vicmr1994@gmail.com>
2020-09-16 15:59:01 +0200

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +10,30 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
 
+  airData : Observable<any>;
+  isLoading = false;
+  data: any;
+
   constructor(
-    private http: HttpClient
-  ) {}
+    private httpClient: HttpClient,public navCtrl: NavController
+  ) {
+    this.isLoading = true;
+    this.airData = this.httpClient.get('https://run.mocky.io/v3/967ef4af-4739-480e-96ca-239888d65b9f');
+    this.airData
+    .subscribe(response => {
+      console.log('my data: ', response);
+      this.data = response.data;
+      this.isLoading = false;
+    });
+
+  }
+
+  dataExample() {
+    //this.airData
+  }
 
   getState() {
-    return this.http.get('');
+    return this.httpClient.get('');
   }
 
 }
