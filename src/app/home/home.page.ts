bd4988d96f8fb94caf87f01a07e2f60303354a19
AppDeveloperV <vicmr1994@gmail.com>
2020-09-18 12:25:44 +0200

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { NavController } from '@ionic/angular';
+import { Observable, Subscription } from 'rxjs';
+import { AlertController, NavController } from '@ionic/angular';
+import { AirDataService } from '../services/air-data.service';
+import { AirConditioner } from './models/air-conditioner.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy{
+  device : AirConditioner;
+  brand : string;
+  model: string;
+  private airSubscription: Subscription;
 
   airData : Observable<any>;
   isLoading = false;
   data: any;
+  temp:number;
 
   constructor(
-    private httpClient: HttpClient,public navCtrl: NavController
+    private airdataService: AirDataService,
+    private httpClient: HttpClient,
+    public navCtrl: NavController,
+    public alertCtrl: AlertController
   ) {
     this.isLoading = true;
     this.airData = this.httpClient.get('https://run.mocky.io/v3/967ef4af-4739-480e-96ca-239888d65b9f');
@@ -24,16 +34,54 @@ export class HomePage {
       console.log('my data: ', response);
       this.data = response.data;
       this.isLoading = false;
+
+      this.setTemp();
     });
 
   }
 
-  dataExample() {
-    //this.airData
+  ngOnInit() {
+    this.isLoading = true;
+
+    this.airSubscription = this.airdataService
+    .getAirConditioner(this.brand, this.model)
+    .subscribe( device => {
+      this.device = device;
+      this.isLoading = false;
+    }, error => {
+      this.alertCtrl
+      .create({
+        header: 'An error ocurred!',
+        message: 'Could not load data. Try again later.',
+        buttons : [{
+          text: 'Okay',
+          //handler: () => {
+            //
+          //}
+        }]
+      }).then(alertEl => {
+        alertEl.present();
+      })
+    })
+  }
+
+  setTemp(){
+    this.temp = parseInt(this.data.temp);
+  }
+
+  tempUp(){
+    this.temp = this.temp + 1;
+    console.log(this.temp);
   }
 
   getState() {
     return this.httpClient.get('');
+  }
+
+  ngOnDestroy() {
+    if (this.airSubscription) {
+      this.airSubscription.unsubscribe();
+    }
   }
 
 }
