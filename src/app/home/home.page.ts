cef6d998d7fbd8b1b052c7acc5520c16ae5a0aff
AppDeveloperVM <vicmr1994@gmail.com>
2020-09-19 18:49:18 +0200

    Home page updated

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,23 +11,28 @@ import { AirConditioner } from './models/air-conditioner.model';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit, OnDestroy{
-  
-  device : AirConditioner;
-  brand : string;
+
+  device: AirConditioner;
+  brand: string;
   model: string;
   private airSubscription: Subscription;
 
   airData: Observable<any>;
   isLoading = false;
-  data: any;
-  temp: number;
+  data: any = [];
+  updatedData: any = [];
+
+  time = '00:00';
+  temp = 0;
+  Mode: any = ['Heat', 'Cold', 'Standard']; //modes
+  fan: any = [];
+
 
   constructor(
     private airdataService: AirDataService,
     private httpClient: HttpClient,
     public navCtrl: NavController,
     public alertCtrl: AlertController
-    
   ) {
     this.isLoading = true;
     this.airData = this.httpClient.get('https://run.mocky.io/v3/967ef4af-4739-480e-96ca-239888d65b9f');
@@ -36,8 +41,8 @@ export class HomePage implements OnInit, OnDestroy{
       console.log('my data: ', response);
       this.data = response.data;
       this.isLoading = false;
-
-      
+      this.updatedData = this.data;
+      this.setTemp();
     });
 
   }
@@ -50,6 +55,7 @@ export class HomePage implements OnInit, OnDestroy{
     .subscribe( device => {
       this.device = device;
       this.isLoading = false;
+
     }, error => {
       this.alertCtrl
       .create({
@@ -57,22 +63,36 @@ export class HomePage implements OnInit, OnDestroy{
         message: 'Could not load data. Try again later.',
         buttons : [{
           text: 'Okay',
-          //handler: () => {
+          // handler: () => {
             //
-          //}
+          // }
         }]
       }).then(alertEl => {
         alertEl.present();
-      })
-    })
+      });
+    });
   }
 
   setTemp(){
-    this.temp = parseInt(this.data.temp);
-    
+    this.temp = this.updatedData.temp;
+    console.log('temp:' + this.temp);
   }
 
-  
+  tempUp(){
+    let newTemp;
+    newTemp = this.temp++;
+    this.updatedData.temp = newTemp;
+    console.log('temp:' + newTemp);
+    return newTemp;
+  }
+
+  tempDown() {
+    let newTemp;
+    newTemp = this.temp--;
+    this.updatedData.temp = newTemp;
+    console.log('temp:' + this.temp);
+    return newTemp;
+  }
 
   getState() {
     return this.httpClient.get('');
